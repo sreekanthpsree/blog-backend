fea88ab0eac20458e28a7b258bd3710bacdfad83
sreekanthpsree <cnct2sree@yahoo.com>
2023-04-22 13:49:32 +0530

    firstcommit

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI:
-    "mongodb+srv://cnct2sree:Y1qMeWkE6YjABnFX@cluster0.h8ejedu.mongodb.net/?retryWrites=true&w=majority",
+  mongoURI: "",
   cookieKey: "ajshgdjasdgajkshdkoafiosfdsjkfhskdjfh",
 };
