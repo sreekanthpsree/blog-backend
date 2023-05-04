ca29c2be69bd8899a23c53d0e534b7fb4a7aef4b
Sreekanth <116191030+sreekanthpsree@users.noreply.github.com>
2023-05-04 14:23:57 +0530

    Update billingRoutes.js

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const User = require("../models/Users");
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY,{
+   timeout: 30000,);
 
 module.exports = (app) => {
   const plans = new Map([
